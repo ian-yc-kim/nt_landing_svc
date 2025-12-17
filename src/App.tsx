21ec3c0fdf733119c76db0258f52d131a84798c2
Thesen AI <aiagent@thesen.ai>
2025-12-17 04:13:36 +0000

    Initial React project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>React Template</h1>
+      <p>Start building your application here.</p>
+    </div>
+  )
+}
+
+export default App
